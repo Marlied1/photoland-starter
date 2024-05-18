04ea6db5f5632f5c00d7ff1e20720139a84cbbc7
Marlie D. <marliedenis3@gmail.com>
2024-05-17 23:36:04 -0400

    .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
+// use fetch hook
+
+import useFetch from '../hooks/useFetch.js';
 
 const Home = () => {
+  const { data } = useFetch('/products?populate=*');
+  console.log(data);
   return <div>Home</div>;
 };
 
